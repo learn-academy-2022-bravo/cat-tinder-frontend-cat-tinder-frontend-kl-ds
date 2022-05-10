ec669276a38193392c4d6ece071674c37ef627a2
kmmontes <kmontes4989@gmail.com>
2022-05-10 15:53:23 -0700

    Installed and configured broswer router and confirmed they work!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Home from './pages/Home'
+import CatIndex from './pages/CatIndex'
+import CatShow from './pages/CatShow'
+import CatNew from './pages/CatNew'
+import CatEdit from './pages/CatEdit'
+import NotFound from './pages/NotFound'
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <>
-        <h1>App Component</h1>
-      </>
+     
+       <Router>
+         <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/catindex" component={CatIndex} />
+            <Route path="/catshow" component={CatShow} />
+            <Route path="/catnew" component={CatNew} />
+            <Route path="/catedit" component={CatEdit} />
+            <Route component={NotFound}/> 
+         </Switch>
+       </Router>
+    
     )
   }
 }
