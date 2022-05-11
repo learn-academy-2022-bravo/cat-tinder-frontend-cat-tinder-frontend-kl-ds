d3711d974652e03d15373f801f7d96d3bdb6f08d
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-05-11 13:44:26 -0500

    Completed header & footer test coverange and verified pass

---
+++ src/components/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Header from './Header'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("When the Header renders", () => {
+    it("shows 2 NavItems", () => {
+        const renderedHeader = shallow(<Header />)
+
+        const renderedNavItems = renderedHeader.find("NavItem")
+
+        expect(renderedNavItems.length).toEqual(2)
+
+    })
+})
